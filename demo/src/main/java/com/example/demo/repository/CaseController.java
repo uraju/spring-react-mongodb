93c18e5e53b0c9c3f0af2d714de9f2f42e7cc669
Umadevi Raju <umadevibala2002@gmail.com>
2017-12-10 21:33:45 -0500

    Enable redis cache

--- demo/src/main/java/com/example/demo/repository/CaseController.java
+++ demo/src/main/java/com/example/demo/repository/CaseController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Case;
 import com.example.demo.service.CaseService;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 
 @CrossOrigin
 @RestController
@@ -17,14 +20,17 @@ import com.example.demo.service.CaseService;
 public class CaseController {
 	@Autowired
 	private  CaseService service;
-	
+        	
     @RequestMapping("/fname/{firstName}")
     //@CachePut(value = "case", key = "#firstName")
+    @Cacheable(value = "case", key = "#firstName", unless = "#result == null")
     public List<Case> findByFirstName(@PathVariable("firstName") String firstName) {
         return service.findByFirstName(firstName);
     }
     @RequestMapping("/lname/{lastName}")
-    //@CachePut(value = "case", key = "#lastName")
+    @CachePut(value = "case", key = "#lastName") // to value to be updated
+    //@CacheEvict(value = "case", key = "#title")  // remove one entry
+    //@CacheEvict(value = "case", allEntries = true) // remove all cache of case
     public List<Case> findByLastName(@PathVariable("lastName") String lastName) {
         return service.findByLastName(lastName);
     }
