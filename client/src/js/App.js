8d5d462140e1d6fe039684e6d1862c21bdfbc72d
Umadevi Raju <uraju@users.noreply.github.com>
2018-01-10 05:49:26 -0500

    Update App.js
    left bottom menu

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -9,18 +9,23 @@ import Home from './components/Home'
 import PageOne from './components/PageOne'
 import PageTwo from './components/PageTwo'
 
+const menuBottom = { // css variable (or a class in css file)
+  position: 'fixed',
+  bottom: '10px',
+}
 class AppMenu extends Component {
   
       constructor() {
           super();
           this.state = { activeMenu: -1 };
   }
+
   render() {
     return (
       <ul className="nav">
         <li><NavLink exact activeClassName="active" to="/" onlyActiveOnIndex>Home</NavLink></li>
         <li><NavLink activeClassName="active" to="/pageone">Page One</NavLink></li>
-        <li><NavLink activeClassName="active" to="/pagetwo">Page Two</NavLink></li>
+        <li style={menuBottom}><NavLink activeClassName="active" to="/pagetwo">Page Two</NavLink></li>
       </ul>
     );
   }
